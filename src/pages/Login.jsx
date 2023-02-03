d4372379ae39d46f913445663858d7bd242242e8
sultzx <quangaliyev@gmail.com>
2023-02-04 02:30:13 +0600

    last

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,37 @@
-const Login = () => {
-    return (<>
-        Login Page
-    </>)
-}
+import { Container, Row, Col, Button, Card } from "react-bootstrap";
 
-export default Login
+import "../styles/Login.scss";
+
+const Login = () => {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col className="col-12 d-flex align-items-center justify-content-center">
+            <Card className="login-card text-center">
+              <Card.Body>
+                <Card.Title>{"Кіру"}</Card.Title>
+                <Row>
+                  <Col className="d-flex column justify-content-center align-items-center">
+                    <Button className="switch-to-client-outline-btn flex-fill">Клиент үшін</Button>
+                  </Col>
+                  <Col className="d-flex column justify-content-center align-items-center">
+                    <Button className="flex-fill">Қызметкер үшін</Button>
+                  </Col>
+                </Row>
+                
+                <Row>
+                <Col className="d-flex column justify-content-center align-items-center">
+                    <input className=" flex-fill" type="text" />
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Login;
